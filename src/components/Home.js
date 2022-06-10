5627c8bb6b92b03307f979ab2cde11cae4e0b843
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-06-10 15:37:07 +0200

    Add Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import shoe_1  from "../images/nike1.png"
+import shoe_2  from "../images/nike2.png"
+import shoe_3  from "../images/nike3.png"
+import {Link} from "react-router-dom"
+
+
 
 export default function Home(){
 
     return(
-        <h1>HOME baby</h1>
+        <main className='home-page'>
+            <h1>Boki's Thrift Shop</h1>
+            <div className='shoes-section'>
+                <img src={shoe_1} alt="shoe"/>
+                <img src={shoe_2} alt='shoe'/>
+                <img src={shoe_3}  alt='shoe'/>
+            </div>
+            
+            <div className='info-section'>
+                <h1>High Quality Shoes for Low Price</h1>
+            </div>
+
+            <Link to='/shop'>
+                <button className='shop--btn'>Shop Now</button>
+            </Link>
+        </main>
     )
 }
