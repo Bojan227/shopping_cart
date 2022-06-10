47890ed64e499189badfc545fee9856c5635b28f
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-06-10 12:54:46 +0200

    Shopping cart app

---
+++ src/components/Nav.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Link} from "react-router-dom"
+
+export default function Nav(){
+
+    return (
+
+        <nav>
+            <ul>
+                <Link to='/'>
+                    <li>Home</li>
+                </Link>
+
+                <Link to='/shop'>
+                    <li>Shop</li>
+                </Link>
+            </ul>
+
+        </nav>
+
+
+    )
+
+}
